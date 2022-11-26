74462657b06acd54032ff5810b6e0976443ea7fc
Ian Gathui <ian.gathui@student.moringaschool.com>
2022-11-26 13:21:22 +0300

    Lab completed

---
+++ index.js
@@ -0,0 +1,5 @@
+const divide = (parameter1 = 2000, parameter2 = 100) => parameter1/parameter2;
+
+const square = (parameter3) => parameter3 * parameter3;
+
+const add = (parameter4, parameter5) => parameter4 + parameter5; 
